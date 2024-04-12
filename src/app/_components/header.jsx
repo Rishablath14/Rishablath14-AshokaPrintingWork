90eb35c6559d253f59f6798078f79edae45524a0
Rishab Lath <reshablath@gmail.com>
2024-04-12 22:56:33 +0530

    Hello

--- src/app/_components/header.jsx
+++ src/app/_components/header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
   return (
     <div className='w-full fixed h-16 shadow-lg bg-slate-50 dark:bg-slate-900 flex justify-between items-center px-4 md:px-8'>
       {/* <Image src="/asp_logo.webp" alt="APW LOGO" fetchPriority='high' width={50} height={50} className='hidden md:block'/> */}
-      <Image src="/asp_logo.webp" alt="APW LOGO" fetchPriority='high' width={50} height={50} className='block mix-blend-luminosity'/>
+      <Image src="/asp_logo.webp" alt="APW LOGO" fetchPriority='high' width={50} height={50} className='block mix-blend-luminosity dark:mix-blend-multiply'/>
       <div className='flex gap-4 md:gap-12 justify-center items-center'>
         {navLinks.map((nav,idx)=>(<Link key={idx} href={nav.link} className={cn('text-lg font-medium dark:border-white border-black px-2 rounded-md',path===nav.link?'border':'')}>{nav.label}</Link>))}
       </div>
