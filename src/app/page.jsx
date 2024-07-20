d79e95e479fcad33949322ca87c5872914c38870
Rishab Lath <reshablath@gmail.com>
2024-07-20 14:31:53 +0530

    revalidattry2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Dashboard from './_components/Dashboard'
 
-export const revalidate = 10;
-
 const page = async () => {
   return (
     <div className='mt-4 mx-2 md:mx-4'>
